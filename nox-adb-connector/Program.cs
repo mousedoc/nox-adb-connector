c54dc9b5419ab34f24da17788ac9dee412789101
mousedoc <sangyun3631@gmail.com>
2021-07-07 15:02:13 +0900

    Create solution

---
+++ nox-adb-connector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nox_adb_connector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
